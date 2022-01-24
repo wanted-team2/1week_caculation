34903ae5c4f46aa95c76666d18a09ddeb5864129
ludacirs <ludacirs@naver.com>
2022-01-25 05:15:30 +0900

    ✨ Feat: PR 테스트 커밋

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react'
-import Api from '@api'
+import React from 'react'
+import { SecondCalculator } from '@components/base'
+import styled from '@emotion/styled'
+
+const HomepageBlock = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`
 
 const HomePage = () => {
-  useEffect(() => {
-    const { data } = Api({
-      url: `live?access_key=${process.env.REACT_APP_ACCESS_KEY}`,
-    })
+  // useEffect(() => {
+  //   const { data } = Api({
+  //     url: `live?access_key=${process.env.REACT_APP_ACCESS_KEY}`,
+  //   })
+  //
+  //   console.log(data)
+  // }, [])
 
-    console.log(data)
-  }, [])
-
-  return <></>
+  return (
+    <HomepageBlock>
+      <SecondCalculator />
+    </HomepageBlock>
+  )
 }
 
 export default HomePage
