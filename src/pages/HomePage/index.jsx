cefabc6ef8f5823976c59364291faf52c81a534e
Jeongs <kimm.jeongs@gmail.com>
2022-01-26 08:39:26 +0900

    :recycle:  Refactor: 주석 정리 및 mockData -> 실제 API 요청으로 변경

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { useAsync } from '@hooks'
-import { getMockData } from '@api' // @NOTE: 임시 mock 데이터 적용
+import { getCurrencyApi, getMockData } from '@api'
 import { FirstCalculator } from '@components/domain'
 import { SecondCalculator } from '@components/base'
 import styled from '@emotion/styled'
-
-// @NOTE: input 값 - 방지
-// @NOTE: onChange 일어나면 value = 0 -> 0 없애주는 처리
 
 const HomePage = () => {
   const {
@@ -14,7 +11,7 @@ const HomePage = () => {
     error,
     data: currencyInfo,
     execute,
-  } = useAsync(getMockData, true)
+  } = useAsync(getCurrencyApi, true) // @NOTE: API 요청 리미트 250회가 넘으면 getMockApi로 변경
 
   return (
     <HomePageBlock>
