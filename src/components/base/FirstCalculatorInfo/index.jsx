11bfa3553c0dad5fe5d85959d556d9aa17ab9b4c
Jeongs <kimm.jeongs@gmail.com>
2022-01-26 10:21:33 +0900

    :bug:  Fix: ErrorText 분기 수정

--- src/components/base/FirstCalculatorInfo/index.jsx
+++ src/components/base/FirstCalculatorInfo/index.jsx
@@ -4,10 +4,14 @@ import styled from '@emotion/styled'
 const FirstCalculatorInfo = ({ totalMoney, activeCurrency, isValidate }) => {
   return (
     <InfoContainer>
-      <p>
-        수취금액은 {addComma(+totalMoney)} {activeCurrency.currency} 입니다.
-      </p>
-      {!isValidate && <ErrorText>송금액이 바르지 않습니다.</ErrorText>}
+      {isValidate ? (
+        <p>
+          수취금액은 {addComma(+totalMoney)} {activeCurrency.currency} 입니다.
+        </p>
+      ) : (
+        <ErrorText>송금액이 바르지 않습니다.</ErrorText>
+      )}
+      {/* {!isValidate && } */}
     </InfoContainer>
   )
 }
